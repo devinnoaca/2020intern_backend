d581b0116fef9ccfdfae2098d29318d3e44b606d
inpyeong <jinipyung@gmail.com>
2020-07-30 14:25:03 +0900

    Correct database information

--- database/pool.js
+++ database/pool.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '10.19.247.204',
   port: 3306,
-  user: 'root',
-  password: 'innovation',
-  database: 'service',
+  user: 'jingoo',
+  password: '1234',
+  database: 'innoacca',
   connectionLimit: 10
 })
 
