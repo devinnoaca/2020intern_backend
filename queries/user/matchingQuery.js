f23ef4fb2b1e9d3f83708594e75fa79d9d3ef913
ssen1994 <ssen1994@naver.com>
2020-08-04 14:50:07 -0700

    modifed query

--- queries/user/matchingQuery.js
+++ queries/user/matchingQuery.js
@@ -1,5 +1,5 @@
 exports.getMatchingList = `SELECT * FROM Matching A 
-    RIGHT OUTER JOIN Matching_keyword B 
+    JOIN Matching_keyword B 
     ON A.matching_ID = B.mk_matching_ID
     WHERE (mentor_USN = ? OR mentee_USN = ?) 
     AND A.matching_state = ?
