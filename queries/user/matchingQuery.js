268f442f3969375bdb7ffd3727249cbb3315d4cf
ssen1994 <ssen1994@naver.com>
2020-08-05 09:37:12 -0700

    query modify

--- queries/user/matchingQuery.js
+++ queries/user/matchingQuery.js
@@ -6,13 +6,13 @@
 //    ORDER BY A.matching_ID ASC
 //    ;`;
 exports.getMentorMatchingList = 
-    `SELECT * FROM get_matching_mentee
-    WHERE (mentor_USN = ? AND matching_state = ?)
-    ORDER BY matching_ID ASC
-    ;`
+`SELECT * FROM get_matching_mentee
+WHERE (mentor_USN = ? AND matching_state = ?)
+ORDER BY matching_ID ASC
+;`
 
 exports.getMenteeMatchingList = 
-    `SELECT * FROM get_matching_mentor
-    WHERE (mentee_USN = ? AND matching_state = ?)
-    ORDER BY matching_ID ASC
-    ;`
+`SELECT * FROM get_matching_mentor
+WHERE (mentee_USN = ? AND matching_state = ?)
+ORDER BY matching_ID ASC
+;`
