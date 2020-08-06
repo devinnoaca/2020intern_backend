14638df9d17f746536000426f4df5c5ca494e829
inpyeong <jinipyung@gmail.com>
2020-08-06 16:32:24 +0900

    Move lib folder into Controller

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,6 @@ const keywordController = require('../controllers/main/keywordController');
 const mentorListController = require('../controllers/main/mentorListController');
 
 router.get('/keyword', keywordController.getKeywordList);
-router.get('/list', mentorListController.getMentorList);
+router.post('/list', mentorListController.getMentorList);
 
 module.exports = router;
