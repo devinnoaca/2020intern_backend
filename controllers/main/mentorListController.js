dd49e6d716729f446aba43608eda55c8695a566c
ssen1994 <ssen1994@naver.com>
2020-08-06 08:48:39 -0700

    modify logic

--- controllers/main/mentorListController.js
+++ controllers/main/mentorListController.js
@@ -1,4 +1,5 @@
 const mentorListDAO = require('../../models/main/mentorListDAO');
+const mentorListLib = require('../../lib/mentorList');
 
 // http://localhost:3001/main/list?keyword0=React.js&keyword1=ELK
 
@@ -10,44 +11,14 @@ const getMentorList = async (req, res, next) => {
   try {
     let _mentorList = await mentorListDAO.getMentorList(keyword);
     let _careerList = await mentorListDAO.getAllCareer();
-    let _career = new Array();
-    let result = new Array();
-    let start = 0;
 
     console.log(_mentorList[0]);
 
-    for (i = 0; i < _careerList[0].length; i++) {
-      if (_careerList[0][i].user_USN === start) {
-        _career[start - 1].career.push(_careerList[0][i].content);
-      } else {
-        _career.push({
-          "usn": start + 1,
-          "career": [_careerList[0][i].content],
-        })
-        start = start + 1;
-      }
-    }
+    let allMentorList = mentorListLib.mentorListLogic(_mentorList, _careerList);
 
-    result.push({
-      mentorList: []
-    })
-
-    for (j = 0; j < _mentorList[0].length; j++) {
-      if (_mentorList[0][j].mentor_USN === _career[j].usn) {
-        result[0].mentorList.push({
-          "usn": _mentorList[0][j].mentor_USN,
-          "name": _mentorList[0][j].user_name,
-          "imageUrl": _mentorList[0][j].image_url,
-          "email": _mentorList[0][j].description,
-          "description": _mentorList[0][j].description,
-          "company": _mentorList[0][j].company,
-          "career": _career[j].career,
-        })
-      }
-    }
 
     //console.log(result[0].mentorList[0]);
-    return res.status(200).send(result[0]);
+    return res.status(200).send(allMentorList);
   } catch (err) {
     return res.status(500).json(err);
   }
