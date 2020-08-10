ed353ef47a0547be251b52ca9c34ff2de0429a63
JunhoYoon95 <junmannnyoon@gmail.com>
2020-08-10 14:46:08 +0900

    lib의 mentorList.js 페이지 변경

--- controllers/lib/mentorList.js
+++ controllers/lib/mentorList.js
@@ -3,7 +3,6 @@ const { ConsoleTransportOptions } = require("winston/lib/winston/transports");
 const mentorListLogic = (_careerList, _order) => {
 	let _career = new Array();
 	let result = new Array();
-
 	let start = 0;
 	for (i = 0; i < _careerList[0].length; i++) {
 		if (_careerList[0][i].user_USN === start) {
@@ -20,12 +19,14 @@ const mentorListLogic = (_careerList, _order) => {
   
   console.log("lib폴더의 mentorList.js의 _career 출력 \n",_career);
 
+
 	result.push({
 		mentorList: []
 	})
-	console.log("lib폴더의 _order[0]의 길이를 출력 \n",_order[0].length);
+  console.log("lib폴더의 _order[0]의 길이를 출력 \n",_order[0].length);
 	for (j = 0; j < _order[0].length; j++) {
 		result[0].mentorList.push({
+      "total":_order[0][j].total_List,
 			"usn": _order[0][j].mentor_USN,
 			"name": _order[0][j].name,
 			"imageUrl": _order[0][j].image_url,
@@ -36,6 +37,8 @@ const mentorListLogic = (_careerList, _order) => {
 		})
 	}
 
+  let total = _order[0][0].total_List;
+  console.log("토탈 진짜 찍어 볼거다",parseInt(total/6) + 1);
 	return result[0];
 }
 
--- models/main/mentorListDAO.js
+++ models/main/mentorListDAO.js
@@ -33,8 +33,8 @@ const getAllCareer = async () => {
 
 const orderMentorList = async (keyword, pageNum) => {
   let query = mentorListQuery.orderMentor;
-  let starting = pageNum*6 -5;
-  
+  let starting = pageNum*6 -5;  // 페이지마다 상위부터 6개씩 고름
+
   query += ` WHERE `
   for (i = 0; i < keyword.length; i++) {
     if (i != keyword.length - 1) {
@@ -44,9 +44,11 @@ const orderMentorList = async (keyword, pageNum) => {
     }
   }
   query += ` GROUP BY name, company, mentor_USN HAVING searched >= 1 ORDER BY searched DESC LIMIT ${starting}, 6;`;
+  // 6개씩
   console.log("pageNum이 몇인지 볼거야", pageNum);
   let data = await conn.connection(query, []);
   return data;  
+  
 }
 
 
