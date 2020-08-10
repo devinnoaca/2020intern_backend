258e82812bf467c08239cf9e29061e9e24350601
inpyeong <jinipyung@gmail.com>
2020-08-10 11:10:32 +0900

    Apply camel case to object property

--- controllers/user/keywordController.js
+++ controllers/user/keywordController.js
@@ -19,9 +19,9 @@ const getKeywords = async (req, res, next) => {
 
 const updateTotalKeywordController = async (req, res, next) => {
   let usn = parseInt(req.params.usn, 10);
-  let keyword_data = req.body.keyword;
-  let insert_data = req.body.keyword.insert_keywords;
-  let delete_data = req.body.keyword.delete_keywords;
+  let keywordData = req.body.keyword;
+  let insertData = req.body.keyword.insertKeywords;
+  let deleteData = req.body.keyword.deleteKeywords;
 
   if (Number.isNaN(usn) || (usn === "undefined") || (usn === "")) {
     return res.status(200).json({ statusCode: 500, message: '잘못된 매개변수 타입' });
@@ -38,7 +38,7 @@ const updateTotalKeywordController = async (req, res, next) => {
 
   if (insert_data.length === 0) {
     console.log("insert_data 없음");
-    let data = [usn, delete_data];
+    let data = [usn, deleteData];
     try {
       let _keyword = await keyword.deleteTotalKeyword(data);
       //console.log(_keyword);
@@ -51,7 +51,7 @@ const updateTotalKeywordController = async (req, res, next) => {
 
   if (delete_data.length === 0) {
     console.log("delete_data 없음")
-    let data = [usn, insert_data];
+    let data = [usn, insertData];
     try {
       let _keyword = await keyword.insertTotalKeyword(data);
       //console.log(_keyword);
@@ -64,7 +64,7 @@ const updateTotalKeywordController = async (req, res, next) => {
 
   try {
     console.log("둘 다 길이가 1 이상");
-    let data = [usn, keyword_data];
+    let data = [usn, keywordData];
     let _keyword = await keyword.updateTotalKeyword(data);
     //console.log(_keyword);
     return res.status(200).send(_keyword);
