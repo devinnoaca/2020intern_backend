0a6e816fa5b28c172a0e54ffb996b7bce2e8f318
inpyeong <jinipyung@gmail.com>
2020-08-12 08:44:35 +0900

    Apply code convention

--- controllers/user/careerController.js
+++ controllers/user/careerController.js
@@ -33,8 +33,42 @@ const handleUserCareerController = async (req, res, next) => {
     return res.status(200).json({ statusCode: 500, message: '값이 없음' });
   }
 
+  let careerBindValue = [],
+      insertCareerQueryBindValue = [],
+      updateCareerQueryBindValueCase = [], updateCareerQueryBindValueWhere = [],
+      deleteCareerQueryBindValue = [];
+
+  for (let i = 0; i < career.length; i++) {
+    if (career[i].content === "" || career[i].type === null) continue;
+
+    switch (career[i].type) {
+      case 0:
+        insertCareerQueryBindValue.push(career[i].content, career[i].user_USN);
+        break;
+      case 1:
+        updateCareerQueryBindValueCase.push(career[i].ID, career[i].content);
+        updateCareerQueryBindValueWhere.push(career[i].ID);
+        break;
+      case 2:
+        deleteCareerQueryBindValue.push(career[i].ID);
+        break;
+    }
+  }
+
+  if (insertCareerQueryBindValue.length) {
+    careerBindValue = careerBindValue.concat(insertCareerQueryBindValue);
+  }
+
+  if (updateCareerQueryBindValueCase.length) {
+    careerBindValue = careerBindValue.concat(updateCareerQueryBindValueCase, updateCareerQueryBindValueWhere);
+  }
+
+  if (deleteCareerQueryBindValue.length) {
+    careerBindValue = careerBindValue.concat(deleteCareerQueryBindValue);
+  }
+  careerBindValue.push(career);
   try {
-    let careerResult = await careerDAO.handleCareerDAO(career);
+    let careerResult = await careerDAO.handleCareerDAO(careerBindValue);
     return res.status(200).send(careerResult);
   } catch (err) {
     return res.status(500).json(err);
