e8e9d52aa19dd43665f79137a9d8549e79ae1c9b
JunhoYoon95 <junmannnyoon@gmail.com>
2020-08-11 15:54:25 +0900

    mentorListDAO의 orderMentorListDAO 고쳤음

--- models/main/mentorListDAO.js
+++ models/main/mentorListDAO.js
@@ -8,7 +8,7 @@ const getAllCareerDAO = async () => {
 
 const orderMentorListDAO = async (keyword, pageNum) => {
   let query = mentorListQuery.orderMentorQuery;
-  let starting = pageNum*6 -6;  // 페이지마다 상위부터 6개씩 고름
+  let starting = (pageNum-1)*6;  // 페이지마다 상위부터 6개씩 고름
 
   query += ` WHERE `
   for (i = 0; i < keyword.length; i++) {
@@ -18,7 +18,7 @@ const orderMentorListDAO = async (keyword, pageNum) => {
       query += `(keyword_ID = "${keyword[i].keywordId}") `;
     }
   }
-  query += ` GROUP BY name, company, mentor_USN HAVING searched >= 1 ORDER BY searched DESC LIMIT ${starting}, ${starting + 6};`;
+  query += ` GROUP BY name, company, mentor_USN HAVING searched >= 1 ORDER BY searched DESC LIMIT ${starting}, 6;`;
   let data = await conn.connection(query, []);
   return data;
 }
