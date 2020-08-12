393b46efa6ade6443dbf62ec8d84eb5d833f55cf
inpyeong <jinipyung@gmail.com>
2020-08-13 08:22:09 +0900

    Use createReqDataObject library in matching create controller and dao

--- controllers/matching/matchingController.js
+++ controllers/matching/matchingController.js
@@ -1,44 +1,49 @@
-const matchingDAO = require('../../models/mathcing/matchingDAO');
+const matchingDAO = require('../../models/matching/matchingDAO');
 const notificationDAO = require('../../models/notification/notificationDAO');
 const dataLib = require('../lib/date');
 const paramsCheck = require('../../lib/paramsCheck');
+const lib = require('../lib/createReqDataObject');
 
 const createMatchingController = async (req, res, next) => {
   let date = new Date();
   let mentorUsn = parseInt(req.body.mentorUsn, 10);
   let menteeUsn = parseInt(req.body.menteeUsn, 10);
   let requestTime = dataLib.getFormatDate(date);
-  let responseTime = null;
+  // let responseTime = null;
   let reqReason = req.body.reqReason;
-  let resReason = "";
+  // let resReason = "";
   let keywordlist = req.body.keywordList;
 
   if(paramsCheck.numberCheck([mentorUsn, menteeUsn]) === false) {
     return res.status(500).json({ statusCode: 500, message: `Cotroller: 정수가 아닌 파라미터` })
-  } 
+  }
   else if(paramsCheck.omissionCheck([mentorUsn, menteeUsn, reqReason, keywordlist]) === false) {
     return res.status(500).json({ statusCode: 500, message: `Cotroller: 파라미터 누락` })
   }
   else {
-    let matchingCreateBindValue = [
-      mentorUsn, menteeUsn, requestTime, responseTime,
-      reqReason, resReason
-    ];
-  
-    let matchingKeywordCreateBindValue = [
-      keywordlist[0].keywordName, keywordlist[0].categoryName
-    ];
-  
-    let notificationCreateBindValue = [
-      null, requestTime, mentorUsn, menteeUsn
-    ]
-  
+    let reqDataObject = lib.createReqDataObject(req.params, req.body);
+    reqDataObject.requestTime = requestTime;
+
+    // let matchingCreateBindValue = [
+    //   mentorUsn, menteeUsn, requestTime, responseTime,
+    //   reqReason, resReason
+    // ];
+
+    // let matchingKeywordCreateBindValue = [
+    //   keywordlist[0].keywordName, keywordlist[0].categoryName
+    // ];
+
+    // let notificationCreateBindValue = [
+    //   null, requestTime, mentorUsn, menteeUsn
+    // ]
+
     try {
-      let matchingResult = await matchingDAO.createMatchingDAO(matchingCreateBindValue);
-      matchingKeywordCreateBindValue.push(matchingResult[0].insertId);
-      notificationCreateBindValue.push(matchingResult[0].insertId);
-      let matchingKeywordResult = await matchingDAO.createMatchingKeywordDAO(matchingKeywordCreateBindValue);
-      let notificationResult = await notificationDAO.createUserNotificationDAO(notificationCreateBindValue);
+      let matchingResult = await matchingDAO.createMatchingDAO(reqDataObject);
+      // matchingKeywordCreateBindValue.push(matchingResult[0].insertId);
+      // notificationCreateBindValue.push(matchingResult[0].insertId);
+      reqDataObject.insertId = matchingResult[0].insertId;
+      let matchingKeywordResult = await matchingDAO.createMatchingKeywordDAO(reqDataObject);
+      let notificationResult = await notificationDAO.createUserNotificationDAO(reqDataObject);
       return res.status(200).send({
         matchingResult, matchingKeywordResult, notificationResult
       });
@@ -57,7 +62,7 @@ const updateMatchingController = async (req, res, next) => {
 
   if(paramsCheck.numberCheck([matchingId, mentorUsn, menteeUsn]) === false) {
     return res.status(500).json({ statusCode: 500, message: `Cotroller: 정수가 아닌 파라미터` })
-  } 
+  }
   else if(paramsCheck.omissionCheck([matchingId, mentorUsn, menteeUsn, resMessage, state]) === false) {
     return res.status(500).json({ statusCode: 500, message: `Cotroller: 파라미터 누락` })
   }
@@ -65,11 +70,11 @@ const updateMatchingController = async (req, res, next) => {
     let date = new Date();
     let resTime = dataLib.getFormatDate(date);
     let modifyMatchingBindValue = [ resMessage, state, resTime, matchingId ];
-  
+
     let notificationCreateBindValue = [
       null, resTime, menteeUsn, mentorUsn, matchingId
     ];
-  
+
     try {
       let result = await matchingDAO.updateMatchingDAO(modifyMatchingBindValue);
       let notificationResult = await notificationDAO.createUserNotificationDAO(notificationCreateBindValue);
--- models/notification/notificationDAO.js
+++ models/notification/notificationDAO.js
@@ -11,12 +11,20 @@ const createNotificationDAO = async (create_data) => {
   return data;
 }
 
-const createUserNotificationDAO = async (create_data) => {
-  if ((create_data === "undefined") || (create_data === "")) {
-    return res.status(200).json({ statusCode: 502, message: '잘못된 매개변수 타입' });
-	}
-  let data = await conn.connection(notificationQuery.insertUserNotificationQuery, create_data);
-  return data;
+const createUserNotificationDAO = async (reqDataObject) => {
+  let notiId = null;
+  let time = reqDataObject.requestTime;
+  let senderUsn = reqDataObject.mentorUsn;
+  let receiverUsn = reqDataObject.menteeUsn;
+  let insertId = reqDataObject.insertId;
+  // if ((create_data === "undefined") || (create_data === "")) {
+  //   return res.status(200).json({ statusCode: 502, message: '잘못된 매개변수 타입' });
+	// }
+  let notificationCreateBindValue = [
+    notiId, time, senderUsn, receiverUsn, insertId
+  ];
+  let DBData = await conn.connection(notificationQuery.insertUserNotificationQuery, notificationCreateBindValue);
+  return DBData;
 }
 
 
