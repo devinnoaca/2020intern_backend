56abbb07737aee2952d372c53d6b142265936a9b
inpyeong <jinipyung@gmail.com>
2020-08-12 21:27:27 +0900

    Apply createReqDataObject library to user matching

--- models/user/matchingDAO.js
+++ models/user/matchingDAO.js
@@ -1,9 +1,11 @@
 const conn = require('../lib/conn');
 const matchingQuery = require('../../queries/user/matchingQuery');
 
-const getMentorMatchingListDAO = async (userMatchingBindValue) => {
-  let usn = userMatchingBindValue[0];
-  let state = userMatchingBindValue[1];
+const getMentorMatchingListDAO = async (reqDataObject) => {
+  // let usn = userMatchingBindValue[0];
+  // let state = userMatchingBindValue[1];
+  let usn = reqDataObject['usn'];
+  let state = reqDataObject['state'];
   if (Number.isNaN(usn) || (Number.isNaN(state))) {
     return res.status(200).json({ statusCode: 502, message: '잘못된 매개변수 타입' });
   }
@@ -15,12 +17,27 @@ const getMentorMatchingListDAO = async (userMatchingBindValue) => {
   if ((usn === "")  || (state === "")) {
     return res.status(200).json({ statusCode: 502, message: '값이 없음' });
   }
-
+  let userMatchingBindValue = [ usn, state ];
   let data = await conn.connection(matchingQuery.getMentorMatchingListQuery, userMatchingBindValue);
   return data;
 }
 
-const getMenteeMatchingListDAO = async (userMatchingBindValue) => {
+const getMenteeMatchingListDAO = async (reqDataObject) => {
+  let usn = reqDataObject['usn'];
+  let state = reqDataObject['state'];
+  if (Number.isNaN(usn) || (Number.isNaN(state))) {
+    return res.status(200).json({ statusCode: 502, message: '잘못된 매개변수 타입' });
+  }
+
+  if ((usn === "undefined") || (state === "undefined")) {
+    return res.status(200).json({ statusCode: 502, message: '잘못된 데이터 형태' });
+  }
+
+  if ((usn === "")  || (state === "")) {
+    return res.status(200).json({ statusCode: 502, message: '값이 없음' });
+  }
+  let userMatchingBindValue = [ usn, state ];
+
   let data = await conn.connection(matchingQuery.getMenteeMatchingListQuery, userMatchingBindValue);
   return data;
 }
