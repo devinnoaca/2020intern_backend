ab8d2e7995381fe43102aab35b29955482944329
inpyeong <jinipyung@gmail.com>
2020-08-20 11:35:34 +0900

    Correct main keyword logic

--- controllers/lib/keyword.js
+++ controllers/lib/keyword.js
@@ -41,6 +41,7 @@ const keywordLogic = (categoryResult, keywordResult) => {
 	// 카테고리 테이블에는 카테고리가 있을 수 있는데 키워드 테이블에 카테고리가 존재 하지 않을 경우 대비해 거꾸로
 	let keywordIndex = keywordList.length - 1;
 	for (j = categoryResult[0].length - 1; j >= 0; j--) {
+    keywordIndex = categoryResult[0][j].ID;
 		if (categoryResult[0][j].ID !== keywordList[keywordIndex].category_ID) {
 			keywordIndex = keywordList[keywordIndex].category_ID;
 			libResult[0].allCategory.push({
@@ -54,7 +55,6 @@ const keywordLogic = (categoryResult, keywordResult) => {
 				"categoryName": categoryResult[0][j].name,
 				"keywordList": keywordList[keywordIndex].keyword
 			})
-			keywordIndex = keywordIndex - 1;
 		}
 	}
 
