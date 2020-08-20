8cc161b782675f5951919d1db313263a4c6dfa90
김정식 <ssen199504@gmail.com>
2020-08-20 13:41:16 -0700

    키워드 lib  수정

--- controllers/lib/keyword.js
+++ controllers/lib/keyword.js
@@ -41,7 +41,6 @@ const keywordLogic = (categoryResult, keywordResult) => {
 	// 카테고리 테이블에는 카테고리가 있을 수 있는데 키워드 테이블에 카테고리가 존재 하지 않을 경우 대비해 거꾸로
 	let keywordIndex = keywordList.length - 1;
 	for (j = categoryResult[0].length - 1; j >= 0; j--) {
-    keywordIndex = categoryResult[0][j].ID;
 		if (categoryResult[0][j].ID !== keywordList[keywordIndex].category_ID) {
 			keywordIndex = keywordList[keywordIndex].category_ID;
 			libResult[0].allCategory.push({
@@ -50,11 +49,15 @@ const keywordLogic = (categoryResult, keywordResult) => {
 				"keywordList": []
 			})
 		} else {
+			console.log("###");
+			console.log(categoryResult[0][j].ID);
+			console.log(keywordList[keywordIndex].category_ID);
 			libResult[0].allCategory.push({
 				"categoryId": categoryResult[0][j].ID,
 				"categoryName": categoryResult[0][j].name,
 				"keywordList": keywordList[keywordIndex].keyword
 			})
+			keywordIndex = keywordIndex - 1;
 		}
 	}
 
